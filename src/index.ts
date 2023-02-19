269eaea9428ec77b7ba51f69784dae9b9ac70c9f
Ronan <ronanmingon69550@gmail.com>
2023-02-19 16:54:58 +0100

    add types

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {Subject} from "rxjs";
 
 let sujects = new Map<string, Subject<any>>()
 export default function Connector(name: string) {
-  return function (target: unknown, key: string) {
+  return function<T>(target: T, key: keyof T) {
 
     Object.defineProperty(target, key, {
       set: (val) => {throw "cannot set this property"},
